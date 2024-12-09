9e3d12e66fb145f9862b8e2f02f73066e9b79171
SIFITarekMazigh <154764130+SIFITarekMazigh@users.noreply.github.com>
2024-12-09 10:51:33 +0100

    Exercice 2

---
+++ Exo2.py
@@ -0,0 +1,9 @@
+tmp = float(input("Please type in the temperature: "))
+
+if tmp < 0 : 
+    print("It's freezing!")
+if tmp < 10 :
+    print("It's cold!")
+if tmp < 20 :
+    print("It's cool!")
+print("Stay safe!")
